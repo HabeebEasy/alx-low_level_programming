3e0756577a59c3cdacfa912b139f7f2d1b4d8815
Habeeb Quadri <quadrihabeeb00@gmail.com>
2023-10-05 21:56:20 -0700

    C work

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main performance
+ *
+ * Return: 0 for approve
+ */
+
+int main(void)
+{
+	printf("Size of a char : %lu byte(s)", sizeof(char));
+	printf("Size of a char : %lu byte(s)", sizeof(int));
+	printf("Size of a char : %lu byte(s)", sizeof(long int));
+	printf("Size of a char : %lu byte(s)", sizeof(long long int));
+	printf("Size of a char : %lu byte(s)", sizeof(float));
