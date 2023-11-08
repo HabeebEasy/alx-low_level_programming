4c0b6745e1dbbdbb9c96d137af17039a08523fce
Habeeb <quadrihabeeb00@gmail.com>
2023-11-07 20:52:47 -0800

    structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stddef.h>
+
+/**
+ * init_dog - initia;ise dog
+ * @d: pointer to dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+		if (d != NULL)
+		{
+			d->name = name;
+			d->age = age;
+			d->owner = owner;
+		}
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - dog structure
+ * @name: name of dpg
+ * @age: age of dog
+ * @owner: owner of owner
+ */
+typedef struct dog
+{
+		char *name;
+		float age;
+		char *owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
