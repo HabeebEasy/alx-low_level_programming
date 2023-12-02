6f220f49558385446704a937d193374ed0a1f35b
HabeebEasy <quadrihabeeb00@gmail.com>
2023-12-01 17:14:37 -0800

    commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,44 @@
-#include "main.h"
-
-/**
- * binary_to_unit - convert a binary to unit
- * @b: binary to convert
- * Return: converted int or 0
+/*
+ * Maes: I am feeling proud today.
  */
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: a pointer to a char string form main function.
+ *
+ * Return: Return: the converted number, or 0 if
+ *  there is one or more chars in the string b that is not 0 or 1
+ *  b is NULL
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsinged int result = 0;
+	int i, j;
+	unsigned int sum = 0;
+	int base;
 
-	if (b == NULL)
-		return (0);
+	i = 0;
 
-	while(*b);
+	while (b[i])
 	{
-		if (*b == '1')/* If the current char is '1', set the bit in the result to 1*/
-			result = (result << 1) | 1;
-
-		/*If the current char is '0', shift the result left by 1 bit*/
-		else if (*b == '0')
-			result <<= 1;
-		else
-			/* if the curent character is not '0' or '1', return 0*/
+		if ((b[i] != '0' && b[i] != '1'))
 			return (0);
-
-		b++;
+		i++;
 	}
-	/* Return the converted unsigned integer*/
-	return (result);
+	i--;
+
+	for (j = i; j >= 0; j--)
+	{
+		if (i == j)
+			base = 1;
+		else
+			base *= 2;
+		if (b[j] & 1)
+			sum += base;
+	}
+	return (sum);
+
+
 }
