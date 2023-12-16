ad7e0772c88bb61033728c32d51a55c6b56d7a39
Habeeb <quadrihabeeb00@gmail.com>
2023-12-15 20:56:29 -0800

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,44 +1,27 @@
-/*
- * Maes: I am feeling proud today.
- */
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
+
 /**
- * binary_to_uint - converts a binary number to an unsigned int.
- * @b: a pointer to a char string form main function.
- *
- * Return: Return: the converted number, or 0 if
- *  there is one or more chars in the string b that is not 0 or 1
- *  b is NULL
- */
+* binary_to_uint - converts a binary number to unsigned int
+*
+* @b: string containing the binary number
+*
+* Return: the decimal number
+*/
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i, j;
-	unsigned int sum = 0;
-	int base;
+	int i;
+	unsigned int decimal = 0;
 
-	i = 0;
+	if (b == NULL)
+		return (0);
 
-	while (b[i])
+	for (i = 0; b[i] != '\0'; ++i)
 	{
-		if ((b[i] != '0' && b[i] != '1'))
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		i++;
+		decimal = 2 * decimal + (b[i] - '0');
 	}
-	i--;
 
-	for (j = i; j >= 0; j--)
-	{
-		if (i == j)
-			base = 1;
-		else
-			base *= 2;
-		if (b[j] & 1)
-			sum += base;
-	}
-	return (sum);
-
-
+	return (decimal);
 }
